d4c8289c1c0682c7beed2b66aed5188db4098e46
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2021-07-10 17:06:35 +0200

    visualize wildfires with heatmap

--- australian_wildfires/visualize_wildfires.py
+++ australian_wildfires/visualize_wildfires.py
@@ -14,6 +14,12 @@ import pandas as pd
 import numpy as np
 import os
 from dotenv import load_dotenv
+import gmaps
+from ipywidgets.embed import embed_minimal_html
+import webbrowser
+import pathlib
+from sklearn import preprocessing
+
 
 # Local Application Modules
 # -----------------------------------------------------------------------------
@@ -25,8 +31,27 @@ print(df.head())
 lon = np.arange(df["longitude"].min(), df["longitude"].max())
 lat = np.arange(df["latitude"].min(), df["latitude"].max())
 
-
+scaler = preprocessing.MinMaxScaler()
+df["brightness_norm"] = scaler.fit_transform(df["brightness"].values.reshape((-1, 1)))
+print(df.head())
 
 load_dotenv()
 
-print(os.getenv("GOOGLE_API_KEY"))
+api_key = os.getenv("GOOGLE_API_KEY")
+
+
+gmaps.configure(api_key=api_key)
+fig = gmaps.figure()
+
+fig.add_layer(gmaps.heatmap_layer(
+    df[["latitude", "longitude"]], 
+    weights=df["brightness_norm"]
+    )
+)
+html_path = "visualization.html"
+embed_minimal_html(html_path, views = [fig])
+
+
+webbrowser.open('file://'+str(pathlib.Path().resolve())+"/"+html_path)
+
+print(fig)
